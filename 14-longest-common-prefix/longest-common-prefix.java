899a6553c0c968b18b29cbb6d640006eb533bf7c
Tbharathraj07 <tarumanibharathraj07@gmail.com>
2025-08-10 17:24:11 +0530

    Time: 7 ms (12.61%) | Memory: 42.3 MB (7.32%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String res="";
+
+        Arrays.sort(strs);
+
+        char [] first = strs[0].toCharArray();
+        char [] last= strs[strs.length-1].toCharArray(); 
+
+        for(int i=0; i<first.length; i++){
+            if(first[i] != last[i]){
+                break;
+            }
+            res+=first[i];
+        }
+        return res.toString();
+    }
+}
