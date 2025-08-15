624692707fdb3747273ff04e71809b9ac5fb02bb
Tbharathraj07 <tarumanibharathraj07@gmail.com>
2025-08-15 15:49:02 +0530

    Time: 4 ms (67.79%) | Memory: 44.8 MB (50.31%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,43 +1,26 @@
 class Solution {
-
-    static int integerval(char ch){
-        if(ch == 'I'){
-            return 1;
-        }else if(ch == 'V'){
-            return 5;
-        }else if(ch == 'X'){
-            return 10;
-        }else if(ch == 'L'){
-            return 50;
-        }else if(ch == 'C'){
-            return 100;
-        }else if(ch == 'D'){
-            return 500;
-        }else if(ch == 'M'){
-            return 1000;
-        }
-        return 0;
-    }
     public int romanToInt(String s) {
         
-        int ans =0;
-        int i=0;
-        for( i=0; i<s.length()-1; i++){
-            char n1=s.charAt(i);
-            char n2=s.charAt(i+1);
-            int n1val=integerval(n1);
-            int n2val=integerval(n2);
-            if(n1val >= n2val){
-                ans+=n1val;
-            }else{
-                ans+=(n2val - n1val);
-                i++;
-            }
-            //System.out.println(ans);
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        hm.put('I',1);
+        hm.put('V',5);
+        hm.put('X',10);
+        hm.put('L',50);
+        hm.put('C',100);
+        hm.put('D',500);
+        hm.put('M',1000);
+
+        int total=0;
+        for(int i=0; i<s.length(); i++){
+            int val=hm.get(s.charAt(i));
+
+            if (i + 1 < s.length() && val < hm.get(s.charAt(i + 1))) {
+                total -= val;
+            } else {
+                total += val;
+            }    
         }
-        if(i == s.length()-1){
-            ans+=integerval(s.charAt(i));
-        }
-        return ans;
+        return total;
     }
 }
