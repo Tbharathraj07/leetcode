0bd35b0bac2173521b00316acb083d913e0f746b
Tbharathraj07 <tarumanibharathraj07@gmail.com>
2025-08-23 01:39:50 +0530

    Time: 333 ms (20.81%) | Memory: 46 MB (14.30%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,35 +1,44 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int maxlen=1;
-        int start=0;
-       if(s == null || s.length() < 1) return "";
-        for(int i=1; i<s.length(); i++){
-
-            int l=i-1;
-            int r=i;
-
-            while(l>=0 && r < s.length() && s.charAt(l) == s.charAt(r)){
-                if(r - l + 1 > maxlen){
-                    maxlen=r-l+1;
-                    start=l;
-                }
-                l--;
-                r++;
+    public String solve(String s,int left,int right){
+        String res="";
+        if(left==right)
+            res+=s.charAt(left);
+        else{
+            if(s.charAt(left)==s.charAt(right)){
+                res+=s.charAt(left);
+                res+=s.charAt(right);
             }
-
-
-            l=i-1;
-            r=i+1;
-
-            while(l>=0 && r < s.length() && s.charAt(l) == s.charAt(r)){
-                if(r-l+1 > maxlen){
-                    maxlen=r-l+1;
-                    start=l;
-                }
-                l--;
-                r++;
+            else
+            {
+                return "";
             }
         }
-            return s.substring(start, start + maxlen);
+        left--;
+        right++;
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            res = s.charAt(left) + res + s.charAt(right);
+            left--;
+            right++;
+        }
+        return res;
+    }
+    public String longestPalindrome(String s) {
+        int n=s.length();
+        String ans="";
+        for(int i=0;i<n;i++){
+            String odd=solve(s,i,i);
+            //System.out.println(odd);
+            if(ans.length()<odd.length())
+            {
+                ans=odd;
+            }
+        }
+        for(int i=0;i<n-1;i++){
+            String even=solve(s,i,i+1);
+            System.out.println(even);
+            if(even.length()>ans.length())
+                ans=even;
+        }
+        return ans;
     }
 }
