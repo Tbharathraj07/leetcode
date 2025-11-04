bab41a8e4fac3778ada7a544980312b692f97a04
Tbharathraj07 <tarumanibharathraj07@gmail.com>
2025-11-04 23:45:55 +0530

    Time: 5 ms (10.30%) | Memory: 44.4 MB (5.70%) - LeetSync

--- 3447-clear-digits/clear-digits.java
+++ 3447-clear-digits/clear-digits.java
@@ -1,26 +1,21 @@
-import java.util.Stack;
-
 class Solution {
     public String clearDigits(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character>st=new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
 
-            if (Character.isDigit(c)) {
-                if (!st.isEmpty()) st.pop(); // remove left non-digit
-                // digit is skipped (removed)
-            } else {
-                st.push(c);
+            if(Character.isDigit(ch)){
+                if(!st.isEmpty())st.pop();
+            }
+            else{
+                st.push(ch);
             }
         }
-
-        // Build final string without StringBuilder
-        String ans = "";
-        for (char c : st) {
-            ans = ans + c;  // concatenation allowed per your request
+        String ans="";
+        for(char i : st){
+            ans+=i;
         }
-
         return ans;
     }
 }
