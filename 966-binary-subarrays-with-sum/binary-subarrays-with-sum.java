7218bcec2c536a6b708422c0671e44dc1d8dff2e
Tbharathraj07 <tarumanibharathraj07@gmail.com>
2025-12-18 10:58:20 +0530

    Time: 1 ms (99.86%) | Memory: 51.1 MB (25.34%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,18 +1,21 @@
 class Solution {
 
-    private int atMost(int[] nums, int goal){
-        int left=0,cnt=0,right=0,sum=0;
-        if(goal < 0)return 0;
-        while(right < nums.length){
+
+    private int atMost(int nums[],int goal){
+
+        int left=0,cnt=0,sum=0;
+        if(goal < 0){
+            return 0;
+        }
+        for(int right=0; right<nums.length; right++){
             sum+=nums[right];
 
             while(sum > goal){
-                sum-=nums[left];
+                sum=sum-nums[left];
                 left++;
             }
-
+            
             cnt=cnt+right-left+1;
-            right++;
         }
         return cnt;
     }
