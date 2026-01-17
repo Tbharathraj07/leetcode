520a4453a53d644f7800223f7efb535d53de6d12
Tbharathraj07 <tarumanibharathraj07@gmail.com>
2026-01-17 15:55:20 +0530

    Time: 9 ms (16.68%) | Memory: 46.7 MB (13.01%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,21 +1,21 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        
-        String ans="";
         int depth=0;
+        String ans="";
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
 
             if(ch == '('){
 
                 if(depth > 0){
-                    ans+=ch;
+                    ans+="(";
                 }
                 depth++;
-            }else{
+            }
+            else{
                 depth--;
                 if(depth > 0){
-                    ans+=ch;
+                    ans+=")";
                 }
             }
         }
